b3df5a9e2bab48e535ad0e5781fffc5bbf9424d4
Jason Dela Torre <jbddelatorre@gmail.com>
2019-05-31 01:02:15 +0800

    cleaned code

--- routes/api/accounts/login.js
+++ routes/api/accounts/login.js
@@ -1,63 +1,62 @@
-const { HttpStatus, router } = require('../base_imports')
+const { HttpStatus, router } = require("../base_imports");
 
-const jwt = require('jsonwebtoken')
-const bcrypt = require('bcryptjs')
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 module.exports = async (req, res) => {
-    const { email, password } = req.body
-    
-    try {
-        const user = await global.db.accounts.users.findOne({ email: email })
-        
-        if(!user) {
-            return res.json({
-                status: HttpStatus.BAD_REQUEST,
-                message: "Email does not exist",
-                body: {}
-            })
-        }
-        
-        const match = await bcrypt.compare(password, user.password)
-        
-        if(match) {
-            const payload = { id: user.id, name: user.name, email: user.email }
+  const { email, password } = req.body;
 
-            jwt.sign(
-                payload,
-                process.env.SECRET_OR_KEY,
-                { expiresIn: 3600 },
-                (err, token) => {
-                    if(err) {
-                        
-                        return res.json({
-                            status: HttpStatus.BAD_REQUEST,
-                            message: "Login failed",
-                            body: {}
-                        })
-                    }
+  try {
+    const user = await global.db.accounts.users.findOne({ email: email });
 
-                    return res.json({
-                        status: HttpStatus.OK,
-                        message: "Login successful",
-                        body: {
-                            ...payload,
-                            token: 'Bearer ' + token
-                        }
-                    })
-                }
-            )
-        } else {
-            return res.json({
-                status: HttpStatus.BAD_REQUEST,
-                message: "Password does not match",
-                body: {}
-            })
-        }
-    } catch(err) {
-        return res.json({
-            status: HttpStatus.INTERNAL_SERVER_ERROR,
-            message: "Database check failed",
-            body: {}
-        })
+    if (!user) {
+      return res.json({
+        status: HttpStatus.BAD_REQUEST,
+        message: "Email does not exist",
+        body: {}
+      });
     }
-}
+
+    const match = await bcrypt.compare(password, user.password);
+
+    if (match) {
+      const payload = { id: user.id, name: user.name, email: user.email };
+
+      jwt.sign(
+        payload,
+        process.env.SECRET_OR_KEY,
+        { expiresIn: 3600 },
+        (err, token) => {
+          if (err) {
+            return res.json({
+              status: HttpStatus.BAD_REQUEST,
+              message: "Login failed",
+              body: {}
+            });
+          }
+
+          return res.json({
+            status: HttpStatus.OK,
+            message: "Login successful",
+            body: {
+              ...payload,
+              token: "Bearer " + token
+            }
+          });
+        }
+      );
+    } else {
+      return res.json({
+        status: HttpStatus.BAD_REQUEST,
+        message: "Password does not match",
+        body: {}
+      });
+    }
+  } catch (err) {
+    return res.json({
+      status: HttpStatus.INTERNAL_SERVER_ERROR,
+      message: "Database check failed",
+      body: {}
+    });
+  }
+};
